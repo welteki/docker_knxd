7d8009aaf193b54b2a192f65c2601e54a0ad5c9b
Han Verstraete <welteki@pm.me>
2020-01-16 20:48:58 +0100

    Pass build args

--- ci.sh
+++ ci.sh
@@ -4,6 +4,8 @@ if [ "$TRAVIS_PULL_REQUEST" = "true" ] || [ "$TRAVIS_BRANCH" != "master" ]; then
   docker buildx build \
     --progress plain \
     --platform=linux/amd64,linux/386,linux/arm/v7,linux/arm/v6,linux/arm64/v8 \
+    --build-arg KNXD_VERSION=$KNXD_VERSION \
+    --build-arg KNXD_RELEASE_TAG=$KNXD_RELEASE_TAG \
     .
   exit $?
 fi
@@ -11,6 +13,8 @@ echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin &> /de
 docker buildx build \
     --progress plain \
     --platform=linux/amd64,linux/386,linux/arm/v7,linux/arm/v6,linux/arm64/v8 \
+    --build-arg KNXD_VERSION=$KNXD_VERSION \
+    --build-arg KNXD_RELEASE_TAG=$KNXD_RELEASE_TAG \
     -t $REPO/$IMAGE_NAME:$KNXD_VERSION
     -t $REPO/$IMAGE_NAME:latest \
     --push \

477b49be21ba897af0eb3f2bc1ea611f55eca68b
Han Verstraete <welteki@pm.me>
2020-01-16 23:21:36 +0100

    Fix: typo in ci script

--- ci.sh
+++ ci.sh
@@ -1,4 +1,4 @@
-!/bin/bash
+#!/bin/bash
 
 if [ "$TRAVIS_PULL_REQUEST" = "true" ] || [ "$TRAVIS_BRANCH" != "master" ]; then
   docker buildx build \
@@ -15,7 +15,7 @@ docker buildx build \
     --platform=linux/amd64,linux/386,linux/arm/v7,linux/arm/v6,linux/arm64/v8 \
     --build-arg KNXD_VERSION=$KNXD_VERSION \
     --build-arg KNXD_RELEASE_TAG=$KNXD_RELEASE_TAG \
-    -t $REPO/$IMAGE_NAME:$KNXD_VERSION
+    -t $REPO/$IMAGE_NAME:$KNXD_VERSION \
     -t $REPO/$IMAGE_NAME:latest \
     --push \
     .
